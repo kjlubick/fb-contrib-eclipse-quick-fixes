cf2c03b6c5f1f8eb5192569bcfe6ec9c2eca416a
Kevin Lubick <kjlubick@ncsu.edu>
2014-08-16 14:36:22 -0400

    Stub LSC resolution
    This is not compilable, or even really close to an Eclipse Plugin.
    Right now, it's a scratchpad, as I'm playing around with modifying the
    FindBugs Eclipse Plugin to get my bearings with the AST fixes.

---
+++ LiteralStringComparisonResolution.java
@@ -0,0 +1,23 @@
+package fb.contrib;
+
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.plugin.eclipse.quickfix.BugResolution;
+import edu.umd.cs.findbugs.plugin.eclipse.quickfix.exception.BugResolutionException;
+
+public class LiteralStringComparisonResolution extends BugResolution {
+
+    @Override
+    protected boolean resolveBindings() {
+        return true;
+    }
+
+    @Override
+    protected void repairBug(ASTRewrite rewrite, CompilationUnit workingUnit, BugInstance bug) throws BugResolutionException {
+        // TODO Auto-generated method stub
+        //rewrite.createStringPlaceholder("FOO BAR", nodeType)
+    }
+
+}
