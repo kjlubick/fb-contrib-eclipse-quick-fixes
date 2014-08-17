714c2bcd9b1f8143d2060f729a3efdb5f0482741
Kevin Lubick <kjlubick@ncsu.edu>
2014-08-16 23:25:50 -0400

    Using visitor to find MethodInvocation
    Also adding debugging

---
+++ Debug.java
@@ -0,0 +1,71 @@
+package fb.contrib;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+/*
+ * fb-contrib - Auxiliary detectors for Java programs
+ * Copyright (C) 2005-2014 Kevin Lubick
+ * Copyright (C) 2005-2014 Dave Brosius
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+public class Debug {
+
+
+	private static PrintStream out;
+
+	static {
+		try {
+			out = new PrintStream(new FileOutputStream("/tmp/eclipse-debug.txt"),true, StandardCharsets.UTF_8.name());
+			out.println("Hello findbugs");
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private Debug(){}
+
+	public static void println() {
+		out.println();
+	}
+
+	public static void println(Object x) {
+		out.println(x);
+	}
+
+	/**
+	 * Like println, but will print PC, if it's passed in
+	 *
+	 * e.g. Debug.println(getPC(), "Hello world");
+	 * will print
+	 * [PC:42] Hello world
+	 *
+	 * @param pc
+	 * @param obj
+	 */
+	public static void println(int pc, Object obj) {
+		out.printf("[PC:%d] %s%n", pc,obj);
+	}
+
+
+
+
+}
--- LiteralStringComparisonResolution.java
+++ LiteralStringComparisonResolution.java
@@ -1,6 +1,14 @@
 package fb.contrib;
 
+import static edu.umd.cs.findbugs.plugin.eclipse.quickfix.util.ASTUtil.getASTNode;
+
+import java.util.List;
+
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.MethodInvocation;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 
 import edu.umd.cs.findbugs.BugInstance;
@@ -18,6 +26,46 @@ public class LiteralStringComparisonResolution extends BugResolution {
     protected void repairBug(ASTRewrite rewrite, CompilationUnit workingUnit, BugInstance bug) throws BugResolutionException {
         // TODO Auto-generated method stub
         //rewrite.createStringPlaceholder("FOO BAR", nodeType)
+        Debug.println("Do work here");
+
+        ASTNode node = getASTNode(workingUnit, bug.getPrimarySourceLineAnnotation());
+        Debug.println(node);
+        LSCVisitor visitor = new LSCVisitor();
+        node.accept(visitor);
+
+        //rewrite.replace(node, replacement, editGroup);
+    }
+
+    private static class LSCVisitor extends ASTVisitor {
+
+        private MethodInvocation LSCMethodInvocation;
+
+        @Override
+        public boolean visit(MethodInvocation node) {
+            Debug.println();
+            Debug.println(node);
+            if (this.LSCMethodInvocation != null) {
+                return false;
+            }
+            Debug.println(node.getName());
+            Debug.println(node.getExpression());
+            Debug.println(node.arguments());
+            if ("equals".equals(node.getName().toString())) {
+                Debug.println("was equals");
+
+                List<Expression> arguments = node.arguments();
+                if (arguments.size() == 1) {
+                    Debug.println(arguments.get(0).resolveConstantExpressionValue());
+                    //if this was a constant string, resolveConstantExpressionValue() will be nonnull
+                    if (null != arguments.get(0).resolveConstantExpressionValue()) {
+                        this.LSCMethodInvocation = node;
+                        return false;
+                    }
+                }
+
+            }
+            return true;
+        }
     }
 
 }
