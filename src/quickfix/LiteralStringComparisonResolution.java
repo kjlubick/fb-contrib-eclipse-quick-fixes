4c1cd8731bb57f154f299cb1c8f8daa01793ad4a
Kevin Lubick <kjlubick@ncsu.edu>
2014-08-18 23:45:49 -0400

    LSC resolution handles other two methods
    I forgot LSC flags compareTo and equalsIgnoreCase, but this will resolve
    them properly now

--- src/quickfix/LiteralStringComparisonResolution.java
+++ src/quickfix/LiteralStringComparisonResolution.java
@@ -2,7 +2,9 @@ package quickfix;
 
 import static edu.umd.cs.findbugs.plugin.eclipse.quickfix.util.ASTUtil.getASTNode;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
@@ -33,23 +35,30 @@ public class LiteralStringComparisonResolution extends BugResolution {
         LSCVisitor visitor = new LSCVisitor();
         node.accept(visitor);
 
-        MethodInvocation badMethodInvocation = visitor.LSCMethodInvocation;
+        MethodInvocation badMethodInvocation = visitor.lscMethodInvocation;
 
         AST ast = rewrite.getAST();
         MethodInvocation fixedMethodInvocation = ast.newMethodInvocation();
-        fixedMethodInvocation.setName(ast.newSimpleName("equals"));
+        fixedMethodInvocation.setName(ast.newSimpleName(visitor.lscMethodInvocation.getName().toString()));
         //can't simply use visitor.stringLiteralExpression because an IllegalArgumentException
         //will be thrown because it belongs to another AST.  So, we use a moveTarget to eventually
         //move the literal into the right place
         fixedMethodInvocation.setExpression((Expression) rewrite.createMoveTarget(visitor.stringLiteralExpression));  //thing the method is called on
-        fixedMethodInvocation.arguments().add(rewrite.createMoveTarget(visitor.stringVariableExpression));
+        fixedMethodInvocation.arguments().add((Expression) rewrite.createMoveTarget(visitor.stringVariableExpression));
 
         rewrite.replace(badMethodInvocation, fixedMethodInvocation, null);
     }
 
     private static class LSCVisitor extends ASTVisitor {
 
-        public MethodInvocation LSCMethodInvocation;
+    	private static Set<String> comparisonMethods = new HashSet<String>(3);
+    	static {
+    		comparisonMethods.add("equals");
+    		comparisonMethods.add("compareTo");
+    		comparisonMethods.add("equalsIgnoreCase");
+    	}
+    	
+        public MethodInvocation lscMethodInvocation;
         public Expression stringLiteralExpression;
         public Expression stringVariableExpression;
 
@@ -57,21 +66,21 @@ public class LiteralStringComparisonResolution extends BugResolution {
         public boolean visit(MethodInvocation node) {
             Debug.println();
             Debug.println(node);
-            if (this.LSCMethodInvocation != null) {
+            if (this.lscMethodInvocation != null) {
                 return false;
             }
             Debug.println(node.getName());
             Debug.println(node.getExpression());
             Debug.println(node.arguments());
-            if ("equals".equals(node.getName().toString())) {
-                Debug.println("was equals");
+            if (comparisonMethods.contains(node.getName().toString())) {
+                Debug.println("was equals, or related");
 
                 List<Expression> arguments = (List<Expression>) node.arguments();
                 if (arguments.size() == 1) {        // I doubt this could be anything other than 1
                     Debug.println(arguments.get(0).resolveConstantExpressionValue());
                     //if this was a constant string, resolveConstantExpressionValue() will be nonnull
                     if (null != arguments.get(0).resolveConstantExpressionValue()) {
-                        this.LSCMethodInvocation = node;
+                        this.lscMethodInvocation = node;
                         this.stringLiteralExpression = arguments.get(0);
                         this.stringVariableExpression = node.getExpression();
                         return false;
