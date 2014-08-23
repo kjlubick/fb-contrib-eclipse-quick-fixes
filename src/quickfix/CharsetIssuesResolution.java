71ba9c9b36ebab0c3fd080e71a4b391c2c3be2e9
Kevin Lubick <kjlubick@ncsu.edu>
2014-08-22 21:00:01 -0400

    Parsing full Signatures into JDT-compatible structures
    I still have to properly handle Methods separately from Constructors

--- src/quickfix/CharsetIssuesResolution.java
+++ src/quickfix/CharsetIssuesResolution.java
@@ -2,14 +2,15 @@ package quickfix;
 
 import static edu.umd.cs.findbugs.plugin.eclipse.quickfix.util.ASTUtil.getASTNode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Map.Entry;
 
 import javax.annotation.Nonnull;
 
+import org.apache.bcel.generic.Type;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
@@ -17,10 +18,7 @@ import org.eclipse.jdt.core.dom.ClassInstanceCreation;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.ConstructorInvocation;
 import org.eclipse.jdt.core.dom.Expression;
-import org.eclipse.jdt.core.dom.ITypeBinding;
 import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
-import org.eclipse.jdt.core.dom.WildcardType;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 
 import util.Debug;
@@ -58,7 +56,7 @@ public class CharsetIssuesResolution extends BugResolution {
 	
 		CSIVisitor csiFinder = findCSIOccurrence(workingUnit, bug);	
 
-        ClassInstanceCreation badMethodInvocation = csiFinder.lscMethodInvocation;
+        ClassInstanceCreation badMethodInvocation = csiFinder.csiConstructorInvocation;
 
         ConstructorInvocation fixedMethodInvocation = createFixedConstructorInvocation(rewrite, csiFinder);
 
@@ -106,52 +104,85 @@ public class CharsetIssuesResolution extends BugResolution {
 	}
 
 	private static class CSIVisitor extends ASTVisitor {
-
-    	private static Map<String, ? extends Object> comparisonMethods;
-    	
-        public ClassInstanceCreation lscMethodInvocation;
-        public String stringLiteralExpression;
-        public int argumentIndex;
-
+	
+		private Map<QTypeAndArgs, Object> csiConstructors;
+		private Map<QTypeAndArgs, Object> csiMethods;
+		
+	    private ClassInstanceCreation csiConstructorInvocation;
+	    private MethodInvocation csiMethodInvocation;
+	    private String stringLiteralExpression;
+	    
+	    
+	    public int argumentIndex;
+	
 		private boolean isName;
 
-        public CSIVisitor(boolean isName) {
+		
+	
+	    public CSIVisitor(boolean isName) {
 			if (isName) {
-				comparisonMethods = CharsetIssues.UNREPLACEABLE_ENCODING_METHODS;
+				parseToTypeArgs(CharsetIssues.UNREPLACEABLE_ENCODING_METHODS);
 			} else {
-				comparisonMethods = CharsetIssues.REPLACEABLE_ENCODING_METHODS;
+				parseToTypeArgs(CharsetIssues.REPLACEABLE_ENCODING_METHODS);
 			}
 			this.isName = isName;
 		}
-        
+	    
+		private void parseToTypeArgs(Map<String, ? extends Object> map) {
+			this.csiConstructors = new HashMap<>();
+			this.csiMethods = new HashMap<>();
+			
+			for(Entry<String, ? extends Object> entry:map.entrySet()) {
+				QTypeAndArgs struct = new QTypeAndArgs(entry.getKey());
+				if (struct.wasConstructor) {
+					csiConstructors.put(struct, entry.getValue());
+				}
+				else {
+					csiMethods.put(struct, entry.getValue());
+				}
+			}
+			
+		}
+		
+		@Override
+		public boolean visit(MethodInvocation node) {
+	        if (foundThingToReplace()) {
+	            return false;
+	        }
+	        return true;
+		}
+		
 		@SuppressWarnings("unchecked")
 		@Override
-        public boolean visit(ClassInstanceCreation node) {
-            if (this.lscMethodInvocation != null) {
-                return false;
-            }
-            ITypeBinding resolveBinding = node.getType().resolveBinding();
-			String invokedConstructorType = resolveBinding.getQualifiedName();
-			if (comparisonMethods.containsKey(invokedConstructorType)) {
-            	List<Expression> arguments = (List<Expression>) node.arguments();
-            		//if this was a constant string, resolveConstantExpressionValue() will be nonnull
-            		int indexOfArgument = getIndexOfArgument(invokedConstructorType);
-            		
-            		if (arguments.size() > indexOfArgument) {
-            			Expression argument = arguments.get(indexOfArgument);
-            			if (null != argument.resolveConstantExpressionValue()) {
-            				this.lscMethodInvocation = node;
-            				this.stringLiteralExpression = (String) argument.resolveConstantExpressionValue();
-            				this.argumentIndex = indexOfArgument;
-            				return false;
-            			}
-            		}
-            }
-            return true;
-        }
+	    public boolean visit(ClassInstanceCreation node) {
+	        if (foundThingToReplace()) {
+	            return false;
+	        }
+			QTypeAndArgs key = new QTypeAndArgs(node);
+			
+			if (csiConstructors.containsKey(key)) {
+				List<Expression> arguments = (List<Expression>) node.arguments();
+				//if this was a constant string, resolveConstantExpressionValue() will be nonnull
+				int indexOfArgument = getIndexOfArgument(key);
+	
+				Expression argument = arguments.get(indexOfArgument);
+				if (null != argument.resolveConstantExpressionValue()) {
+					this.csiConstructorInvocation = node;
+					this.stringLiteralExpression = (String) argument.resolveConstantExpressionValue();
+					this.argumentIndex = indexOfArgument;
+					return false;
+				}
+	
+			}
+	        return true;
+	    }
 
-		private int getIndexOfArgument(String invokedMethodName) {
-			Object indexVal = comparisonMethods.get(invokedMethodName);
+		private boolean foundThingToReplace() {
+			return this.csiConstructorInvocation != null || this.csiMethodInvocation != null;
+		}
+	
+		private int getIndexOfArgument(QTypeAndArgs key) {
+			Object indexVal = csiConstructors.get(key);
 			if (isName && indexVal instanceof Integer) {
 				return (Integer) indexVal;
 			}
@@ -160,6 +191,89 @@ public class CharsetIssuesResolution extends BugResolution {
 			}
 			return -1;	//shouldn't happen
 		}
-    }
+	}
+
+	private static class QTypeAndArgs {
+		public boolean wasConstructor;
+		final String qualifiedType;
+		final List<String> argumentTypes = new ArrayList<String>();
+		
+		//expecting in form "java/io/InputStreamReader.<init>(Ljava/io/InputStream;Ljava/lang/String;)V"
+		public QTypeAndArgs(String fullSignatureWithArgs) {
+			int splitIndex = fullSignatureWithArgs.indexOf('(');
+			String qualifiedTypeWithSlashes = fullSignatureWithArgs.substring(0, splitIndex);
+			
+			qualifiedType = qualifiedTypeWithSlashes.replace('/', '.');
+			
+			Type[] bcelTypes = Type.getArgumentTypes(fullSignatureWithArgs.substring(splitIndex));
+			
+			for(Type t: bcelTypes) {
+				argumentTypes.add(t.toString());		//toString returns them in human-readable dot notation
+			}
+			
+			wasConstructor = fullSignatureWithArgs.contains("<init>");
+		}
+		
+		private QTypeAndArgs(String qualifiedName, List<Expression> arguments) {
+			this.qualifiedType = qualifiedName;
+			for(Expression type: arguments) {
+				argumentTypes.add(type.resolveTypeBinding().getQualifiedName());
+			}
+		}
+	
+		@SuppressWarnings("unchecked")
+		public QTypeAndArgs(ClassInstanceCreation node) {
+			this(node.getType().resolveBinding().getQualifiedName()+".<init>",
+					node.arguments());
+			wasConstructor = true;
+		}
+		
+		public QTypeAndArgs(MethodInvocation node) {
+			this(node.resolveTypeBinding().getQualifiedName(),
+					node.arguments());
+			wasConstructor = false;
+		}
+	
+		@Override
+		public String toString() {
+			return "QTypeAndArgs [qualifiedType=" + qualifiedType + ", argumentTypes=" + argumentTypes + "]";
+		}
+	
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((argumentTypes == null) ? 0 : argumentTypes.hashCode());
+			result = prime * result + ((qualifiedType == null) ? 0 : qualifiedType.hashCode());
+			result = prime * result + (wasConstructor ? 1231 : 1237);
+			return result;
+		}
+	
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			QTypeAndArgs other = (QTypeAndArgs) obj;
+			if (argumentTypes == null) {
+				if (other.argumentTypes != null)
+					return false;
+			} else if (!argumentTypes.equals(other.argumentTypes))
+				return false;
+			if (qualifiedType == null) {
+				if (other.qualifiedType != null)
+					return false;
+			} else if (!qualifiedType.equals(other.qualifiedType))
+				return false;
+			if (wasConstructor != other.wasConstructor)
+				return false;
+			return true;
+		}
+		
+		
+	}
 
 }
