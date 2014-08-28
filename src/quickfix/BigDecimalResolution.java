5720837ca3b60ec32ff26ea7e372df024959a6df
Kevin Lubick <kjlubick@ncsu.edu>
2014-08-27 22:49:02 -0400

    Fixing BigDecimal problem stub

---
+++ src/quickfix/BigDecimalResolution.java
@@ -0,0 +1,109 @@
+package quickfix;
+
+import static edu.umd.cs.findbugs.plugin.eclipse.quickfix.util.ASTUtil.getASTNode;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.plugin.eclipse.quickfix.BugResolution;
+import edu.umd.cs.findbugs.plugin.eclipse.quickfix.exception.BugResolutionException;
+
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.ClassInstanceCreation;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.NumberLiteral;
+import org.eclipse.jdt.core.dom.SimpleName;
+import org.eclipse.jdt.core.dom.SimpleType;
+import org.eclipse.jdt.core.dom.StringLiteral;
+import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+
+public class BigDecimalResolution extends BugResolution {
+
+    private boolean useConstructor;
+
+    @Override
+    protected boolean resolveBindings() {
+        return true;
+    }
+    
+    @Override
+    public void setOptions(@Nonnull Map<String, String> options) {
+        useConstructor = Boolean.parseBoolean(options.get("useConstructor"));
+    }
+
+    @Override
+    protected void repairBug(ASTRewrite rewrite, CompilationUnit workingUnit, BugInstance bug) throws BugResolutionException {
+        ASTNode node = getASTNode(workingUnit, bug.getPrimarySourceLineAnnotation());
+        BigDecimalVisitor visitor = new BigDecimalVisitor();
+        node.accept(visitor);
+
+        Expression fixedExpression = makeFixedExpression(rewrite, visitor);
+        rewrite.replace(visitor.badBigDecimalConstructor, fixedExpression, null);
+    }
+    
+    
+    @SuppressWarnings("unchecked")
+    private Expression makeFixedExpression(ASTRewrite rewrite, BigDecimalVisitor visitor) {
+        if (useConstructor) {
+            return makeConstructor(rewrite, visitor);
+        }
+        
+        AST ast = rewrite.getAST();
+        MethodInvocation fixedMethod = ast.newMethodInvocation();
+        fixedMethod.setName(ast.newSimpleName("valueOf"));
+        SimpleName staticType = ast.newSimpleName("BigDecimal");
+        fixedMethod.setExpression(staticType);
+        fixedMethod.arguments().add(rewrite.createMoveTarget(visitor.decimalVar));
+        
+        return fixedMethod;
+    }
+
+
+    private Expression makeConstructor(ASTRewrite rewrite, BigDecimalVisitor visitor) {
+        AST ast = rewrite.getAST();
+        ClassInstanceCreation fixedConstructor = (ClassInstanceCreation) rewrite.createCopyTarget(visitor.badBigDecimalConstructor);
+        
+        fixedConstructor.arguments().clear();
+        StringLiteral stringLiteral = ast.newStringLiteral();
+        stringLiteral.setLiteralValue(visitor.decimalVar.getToken());
+        fixedConstructor.arguments().add(stringLiteral);
+        return fixedConstructor;
+    }
+
+
+    private static class BigDecimalVisitor extends ASTVisitor {
+        
+        public ClassInstanceCreation badBigDecimalConstructor = null;
+        private NumberLiteral decimalVar;
+        
+        @Override
+        public boolean visit(ClassInstanceCreation node) {
+            if (badBigDecimalConstructor != null) {
+                return false;
+            }
+            Type type = node.getType();
+            if (type instanceof SimpleType
+                    && "java.math.BigDecimal".equals(((SimpleType) type).getName().getFullyQualifiedName())) {
+                
+                @SuppressWarnings("unchecked")
+                List<Expression> args = node.arguments();
+                if (args.size() == 1 && args.get(0) instanceof NumberLiteral) {
+                    badBigDecimalConstructor = node;
+                    this.decimalVar = (NumberLiteral)node.arguments().get(0);
+                }  
+            }
+            
+            return true;
+        }
+        
+    }
+
+}
