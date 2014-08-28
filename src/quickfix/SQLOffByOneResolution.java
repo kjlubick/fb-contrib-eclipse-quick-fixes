19008c1ad33604cf19438dc68feb2168df600cf9
Kevin Lubick <kjlubick@ncsu.edu>
2014-08-28 10:29:50 -0400

    refactored validating method
    I hadn't realized there were so many corner cases.

--- src/quickfix/SQLOffByOneResolution.java
+++ src/quickfix/SQLOffByOneResolution.java
@@ -20,6 +20,7 @@ import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.Expression;
 import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.NumberLiteral;
 import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 
@@ -65,12 +66,37 @@ public class SQLOffByOneResolution extends BugResolution {
         @Override
         public boolean visit(MethodInvocation node) {
             
-            if (types.contains(node.getExpression().resolveTypeBinding().getQualifiedName())) {
+            if (isValidSQLSetUpdateOrGet(node)) {
                 badMethodInvocations.add(node);
                 fixedMethodInvocations.add(makeFixedMethodInvocation(node));
             }
             
             return true;
+        }
+
+
+        private boolean isValidSQLSetUpdateOrGet(MethodInvocation node) {
+            String typeName = node.getExpression().resolveTypeBinding().getQualifiedName();
+            if (!types.contains(typeName)) {
+                return false;
+            }
+            //ResultSet and PreparedStatement have lots of methods
+            //we don't want to change the wrong ones
+            String calledMethod = node.getName().getFullyQualifiedName();
+            if (typeName.contains("PreparedStatement")) {
+                //e.g. setInteger(int paramIndex, int val)
+                return calledMethod.startsWith("set") && node.arguments().size() == 2;
+            }
+
+            List<?> args = node.arguments();
+            if  (!(calledMethod.startsWith("get") && args.size() == 1) ||
+                    (calledMethod.startsWith("update") && args.size() == 2)) {
+                return false;
+            }
+            //there are two versions of resultSet.getString [and updateString]
+            //getString(String colName) and getString(int colIndex)
+            //we only want to update the latter
+            return args.get(0) instanceof NumberLiteral;
         }
 
 
@@ -90,13 +116,14 @@ public class SQLOffByOneResolution extends BugResolution {
         ps.setString(0, "foo");
         ps.setString(1, "bar");
         ps.setInt(2, 42);
+        
     }
     
     
     public void getquery(ResultSet rs) throws SQLException {
         System.out.println(rs.getInt(0));
         System.out.println(rs.getString(1));
-        System.out.println(rs.getInt(2));
+        System.out.println(rs.getInt(2));        
     }
 
 }
