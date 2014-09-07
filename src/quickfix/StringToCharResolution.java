d93011ce928be70fbb6e466502250c852bcd06ef
Kevin Lubick <kjlubick@ncsu.edu>
2014-09-07 19:55:27 -0400

    Setting up datastructure

--- src/quickfix/StringToCharResolution.java
+++ src/quickfix/StringToCharResolution.java
@@ -1,9 +1,12 @@
 package quickfix;
 
+import java.util.List;
+
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.plugin.eclipse.quickfix.BugResolution;
 import edu.umd.cs.findbugs.plugin.eclipse.quickfix.exception.BugResolutionException;
 
+import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 
@@ -21,12 +24,29 @@ public class StringToCharResolution extends BugResolution {
     }
     
     
-    StringBuffer sb;
+    private static class StringToCharVisitor extends ASTVisitor {
+        //private static 
+    }
     
-    private void test(String foo) {
+    
+    //TODO I think I can make this a util class, (something similar is used in charSetIssues)
+    //Subclasses can take different types into the constructor.  
+    private static class QMethodAndArgs {
+        //TODO make these final
+        public String qualifiedTypeString;      //the type that this method is invoked on (dot seperated)
+        public String invokedMethodString;      //the name of the method invoked
+        public List<String> argumentTypes;      //dot seperated argument types
+        
+    }
+    
+    
+    //for testing
+    private StringBuffer sb;
+    
+    public void test(String foo) {
         sb = new StringBuffer();
-        sb.append("f");
-        sb.append('f');
+        sb.append("f").append("o");
+        sb.append('f').append("o");
         StringBuilder sb2 = new StringBuilder();
         sb2.append("g");
         sb2.append('g');
