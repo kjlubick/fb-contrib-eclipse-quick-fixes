eb1f225b31683911156682469e8035b4ef14787d
Kevin Lubick <kjlubick@ncsu.edu>
2014-09-08 23:19:04 -0400

    Proof of concept UCPM resolution working
    Oddly enough, I don't think I have to use anything more complicated than
    subbing out length 1 string literals for characters.
    
    I guess the refactoring will be good for another day, yeah?
    
    I like this solution because it fixes corner cases like
    ```System.out.println("a"+"b"+"c");```
    and fixes them all in one invocation of the resolution.  I'm not sure
    how I would have handled this otherwise.

--- src/quickfix/StringToCharResolution.java
+++ src/quickfix/StringToCharResolution.java
@@ -1,13 +1,22 @@
 package quickfix;
 
+import static edu.umd.cs.findbugs.plugin.eclipse.quickfix.util.ASTUtil.getASTNode;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.plugin.eclipse.quickfix.BugResolution;
 import edu.umd.cs.findbugs.plugin.eclipse.quickfix.exception.BugResolutionException;
 
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CharacterLiteral;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.StringLiteral;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
 
 public class StringToCharResolution extends BugResolution {
@@ -19,13 +28,35 @@ public class StringToCharResolution extends BugResolution {
 
     @Override
     protected void repairBug(ASTRewrite rewrite, CompilationUnit workingUnit, BugInstance bug) throws BugResolutionException {
-        // TODO Auto-generated method stub
-
+        ASTNode node = getASTNode(workingUnit, bug.getPrimarySourceLineAnnotation());
+        
+        StringToCharVisitor visitor = new StringToCharVisitor();
+        node.accept(visitor);
+        
+        AST ast = rewrite.getAST();
+        
+        for(Map.Entry<StringLiteral, Character> entry: visitor.replacements.entrySet()) {
+            CharacterLiteral charLiteral = ast.newCharacterLiteral();
+            charLiteral.setCharValue(entry.getValue());
+            
+            rewrite.replace(entry.getKey(), charLiteral, null);
+        }
     }
     
     
     private static class StringToCharVisitor extends ASTVisitor {
-        //private static 
+        
+        Map<StringLiteral, Character> replacements = new HashMap<>();
+
+        @Override
+        public boolean visit(StringLiteral node) {
+            String literalValue = node.getLiteralValue();
+            if (literalValue.length() == 1) {
+                replacements.put(node, literalValue.charAt(0));
+            }
+            return true;
+        }
+        
     }
    
     
