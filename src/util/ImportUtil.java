89bc30c32f7942eed32098a6ec7d89e35bf6f9dc
Kevin Lubick <kjlubick@ncsu.edu>
2014-11-06 10:36:20 -0500

    Test Environment is now spaces not tabs
    This matches the dev environment, which means we don't get pedantic
    failures anymore.
    
    Also some more Utils

---
+++ src/util/ImportUtil.java
@@ -0,0 +1,19 @@
+package util;
+
+import java.util.ArrayList;
+
+public class ImportUtil {
+
+    public static String[] filterOutJavaLangImports(String[] addedImports) {
+        
+        ArrayList<String> filteredImports = new ArrayList<>();
+        for(String oldImport:addedImports) {
+            if (oldImport.startsWith("java.lang")) {
+                continue;
+            }
+            filteredImports.add(oldImport);
+        }
+        return filteredImports.toArray(new String[0]);
+    }
+
+}
