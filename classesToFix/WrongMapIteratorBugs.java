615361ecd3b15c2b1ccea42577119a458fd2e87f
Kevin Lubick <kjlubick@ncsu.edu>
2014-11-06 10:34:53 -0500

    Adds test cases for WMI

--- classesToFix/WrongMapIteratorBugs.java
+++ classesToFix/WrongMapIteratorBugs.java
@@ -1,4 +1,6 @@
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class WrongMapIteratorBugs {
@@ -13,6 +15,19 @@ public class WrongMapIteratorBugs {
         for(String key: map.keySet()) {
             Integer i = map.get(key);
             System.out.println(key + ':'+i);
+        }
+    }
+    
+    public void testIteratorExplicitList(Map<String, List<Integer>> map) {
+        for (String key : map.keySet()) {
+            List<Integer> someVal = map.get(key);
+            System.out.println(someVal);
+        }
+    }
+    
+    public void testIteratorImplicitSet(Map<String, Set<String>> map) {
+        for (String key : map.keySet()) {
+            System.out.println(map.get(key));
         }
     }
 }
--- fixedClasses/WrongMapIteratorBugs.java
+++ fixedClasses/WrongMapIteratorBugs.java
@@ -1,12 +1,15 @@
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 
 public class WrongMapIteratorBugs {
-    
+  
     public void testIterator(Map<String, Integer> map){
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
-            String key = entry.getKey()
-            System.out.println(key);
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String key = entry.getKey();
+            Integer integer = entry.getValue();
+            System.out.println(integer);
         }
     }
     
@@ -17,5 +20,20 @@ public class WrongMapIteratorBugs {
             System.out.println(key + ':'+i);
         }
     }
-
+    
+    public void testIteratorExplicitList(Map<String, List<Integer>> map) {
+        for (Map.Entry<String, List<Integer>> entry : map.entrySet()) {
+            String key = entry.getKey();
+            List<Integer> someVal = entry.getValue();
+            System.out.println(someVal);
+        }
+    }
+    
+    public void testIteratorImplicitSet(Map<String, Set<String>> map) {
+        for (Map.Entry<String, Set<String>> entry : map.entrySet()) {
+            String key = entry.getKey();
+            Set<String> mapValue = entry.getValue();
+            System.out.println(mapValue);
+        }
+    }
 }
