f812eee61a31bda15088940f0d5493a1fb08e4d5
Kevin Lubick <kjlubick@ncsu.edu>
2014-12-03 18:40:58 -0500

    Adds documentation to the special resolutions
    Both IGNORE_FIX and FIXED_BY_ANOTHER_FIX.  The distinction is subtle.

--- test/utils/QuickFixTestPackage.java
+++ test/utils/QuickFixTestPackage.java
@@ -7,8 +7,17 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 @SuppressFBWarnings(value = "UWF_NULL_FIELD", justification = "This is a struct class.")
 public class QuickFixTestPackage {
 
+    /**
+     * This indicates that we should skip this marker, usually because no quickfix applies.
+     */
     public static final int IGNORE_FIX = -1;
 
+    /**
+     * This indicates that the next regular quickfix invocation will also make this marker go away.
+     * An example is if you have two bugs in an if statement, but they are on different lines,
+     * so two markers are reported. The preferred way to handle this is to fix both problems with
+     * one invocation, so the first should be flagged as handled by the invocation of the second.
+     */
     public static final int FIXED_BY_ANOTHER_FIX = -2;
 
     public String expectedPattern = null;
