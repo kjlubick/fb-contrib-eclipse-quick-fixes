d30caae80ef5c49ec0272b8d335bc6d81cf00680
Kevin Lubick <kjlubick@ncsu.edu>
2014-12-18 11:51:59 -0500

    Adds tests for formatStringBugs
    See #25

---
+++ classesToFix/FormatStringBugs.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+
+
+public class FormatStringBugs {
+
+    public String stuff(String[] args, String thing) {
+        System.out.printf("%s%n", new int[] { 1, 2 });
+        System.out.printf("%b%n", thing);
+        System.out.printf("%d %d %s %n", 5, Boolean.FALSE, thing);
+        System.out.printf("%x%n", Arrays.toString(args));
+        
+        return String.format("\nSize: %d contents: %s \n", args.length, Arrays.asList(args));
+    }
+
+}
