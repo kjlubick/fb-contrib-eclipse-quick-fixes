6366a1e3aa24abd287e7f3911709a017666189e4
Kevin Lubick <kjlubick@ncsu.edu>
2015-01-10 11:28:46 -0500

    Adds stub for CompareFloatResolution

--- src/quickfix/CompareFloatResolution.java
+++ src/quickfix/CompareFloatResolution.java
@@ -1,11 +1,21 @@
 package quickfix;
 
+import static edu.umd.cs.findbugs.plugin.eclipse.quickfix.util.ASTUtil.getASTNode;
+
 import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.plugin.eclipse.quickfix.ApplicabilityVisitor;
 import edu.umd.cs.findbugs.plugin.eclipse.quickfix.BugResolution;
 import edu.umd.cs.findbugs.plugin.eclipse.quickfix.exception.BugResolutionException;
 
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ConditionalExpression;
+import org.eclipse.jdt.core.dom.VariableDeclaration;
+import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
+
+import util.TraversalUtil;
 
 public class CompareFloatResolution extends BugResolution {
 
@@ -13,11 +23,39 @@ public class CompareFloatResolution extends BugResolution {
     protected boolean resolveBindings() {
         return true;
     }
+    
+    @Override
+    protected ASTVisitor getApplicabilityVisitor() {
+        return new CompareToVisitor();
+    }
 
     @Override
     protected void repairBug(ASTRewrite rewrite, CompilationUnit workingUnit, BugInstance bug) throws BugResolutionException {
-        // TODO Auto-generated method stub
+        ASTNode node = getASTNode(workingUnit, bug.getPrimarySourceLineAnnotation());
+        node = TraversalUtil.backtrackToBlock(node);
+        CompareToVisitor visitor = new CompareToVisitor();
+        node.accept(visitor);
+        
+        if (visitor.expressionToReplace != null) {
+            
+        }
+    }
+    
+    private static class CompareToVisitor extends ASTVisitor implements ApplicabilityVisitor {
+        
+        ConditionalExpression expressionToReplace;
+        VariableDeclarationStatement optionalTempVariableToDelete;
+        
+        @Override
+        public boolean visit(ConditionalExpression node) {
+            // TODO Auto-generated method stub
+            return super.visit(node);
+        }
 
+        @Override
+        public boolean isApplicable() {
+            return expressionToReplace != null;
+        }
     }
 
 }
