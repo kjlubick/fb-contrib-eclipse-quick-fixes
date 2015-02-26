d61d8df55cf8e0a477c70096e064a21e53d5de04
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-26 14:27:37 -0500

    Adds script to download eclipse-relang
    (and cache it so we aren't pulling it from the (slow) git repo every
    time).

---
+++ scripts/download_eclipse.sh
@@ -0,0 +1,5 @@
+if [ ! -d "~/eclipse-relang" ]; then
+  # Control will enter here if $DIRECTORY doesn't exist.
+  wget http://git.eclipse.org/c/platform/eclipse.platform.releng.basebuilder.git/snapshot/R38M6PlusRC3G.tar.gz -O - | tar -xz --directory ~
+  mv ~/R38M6PlusRC3G ~/eclipse-relang
+fi
