3bc9e810e869dd23a5580d8168787d1dd1bd5649
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-26 15:13:57 -0500

    Adds debugging for travis ci

--- scripts/download_eclipse.sh
+++ scripts/download_eclipse.sh
@@ -1,6 +1,9 @@
+echo $(pwd)
+echo $(ll -r ..)
 if [ ! -d "../eclipse-relang" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   wget http://git.eclipse.org/c/platform/eclipse.platform.releng.basebuilder.git/snapshot/R38M6PlusRC3G.tar.gz -O - | tar -xz --directory ../
   mv ../R38M6PlusRC3G ../eclipse-relang
   echo $(pwd)
+  echo $(ll -r ..)
 fi
