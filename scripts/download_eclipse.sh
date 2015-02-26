0d442e61ec748dd9cd990cbdcc64449327e48136
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-26 15:27:08 -0500

    Attempts to handle empty eclipse-relang
    Which may happen because of travisci caching?

--- scripts/download_eclipse.sh
+++ scripts/download_eclipse.sh
@@ -1,7 +1,10 @@
 echo $(pwd)
 echo $(ls -lR ..)
+if [ ! -d "../eclipse-relang/plugins" ]; then
+  rm -rf ../eclipse-relang
+fi
+
 if [ ! -d "../eclipse-relang" ]; then
-  # Control will enter here if $DIRECTORY doesn't exist.
   wget http://git.eclipse.org/c/platform/eclipse.platform.releng.basebuilder.git/snapshot/R38M6PlusRC3G.tar.gz -O - | tar -xz --directory ../
   mv ../R38M6PlusRC3G ../eclipse-relang
   echo $(pwd)
