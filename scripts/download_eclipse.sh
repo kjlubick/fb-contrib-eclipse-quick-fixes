9b1223abf4f95876bcf43830cf8ddb229f3534dc
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-26 14:52:12 -0500

    Corrects Eclipse location

--- scripts/download_eclipse.sh
+++ scripts/download_eclipse.sh
@@ -1,5 +1,5 @@
 if [ ! -d "~/eclipse-relang" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   wget http://git.eclipse.org/c/platform/eclipse.platform.releng.basebuilder.git/snapshot/R38M6PlusRC3G.tar.gz -O - | tar -xz --directory ../../
-  mv ~/R38M6PlusRC3G ~/eclipse-relang
+  mv ../../R38M6PlusRC3G ../../eclipse-relang
 fi
