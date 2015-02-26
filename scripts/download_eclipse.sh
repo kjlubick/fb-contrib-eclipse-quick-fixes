a780a5cfa6214f213be8f749290911e30612b16b
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-26 15:21:17 -0500

    More debugging

--- scripts/download_eclipse.sh
+++ scripts/download_eclipse.sh
@@ -1,9 +1,9 @@
 echo $(pwd)
-echo $(ls -lr ..)
+echo $(ls -lR ..)
 if [ ! -d "../eclipse-relang" ]; then
   # Control will enter here if $DIRECTORY doesn't exist.
   wget http://git.eclipse.org/c/platform/eclipse.platform.releng.basebuilder.git/snapshot/R38M6PlusRC3G.tar.gz -O - | tar -xz --directory ../
   mv ../R38M6PlusRC3G ../eclipse-relang
   echo $(pwd)
-  echo $(ll -r ..)
+  echo $(ls -lR ..)
 fi
