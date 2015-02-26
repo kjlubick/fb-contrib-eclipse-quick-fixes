07b0c203804b265f2ae591ef31061e1e1f285c6b
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-26 15:32:22 -0500

    Removing debugging information

--- scripts/download_eclipse.sh
+++ scripts/download_eclipse.sh
@@ -1,12 +1,9 @@
-echo $(pwd)
-echo $(ls -lR ..)
 if [ ! -d "../eclipse-relang/plugins" ]; then
+  echo "Blank eclipse-relang folder found... deleting"
   rm -rf ../eclipse-relang
 fi
 
 if [ ! -d "../eclipse-relang" ]; then
   wget http://git.eclipse.org/c/platform/eclipse.platform.releng.basebuilder.git/snapshot/R38M6PlusRC3G.tar.gz -O - | tar -xz --directory ../
   mv ../R38M6PlusRC3G ../eclipse-relang
-  echo $(pwd)
-  echo $(ls -lR ..)
 fi
