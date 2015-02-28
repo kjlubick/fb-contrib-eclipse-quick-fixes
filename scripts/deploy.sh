9c039b6f949bc99f6bb3937e2dd2f2127921cab9
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-28 11:32:20 -0500

    Creates a new version in bintray

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,3 +4,8 @@ versionJson=`curl -X GET -u kjlubick:$API_KEY https://api.bintray.com/packages/k
 #echo $versionJson
 current_version=`echo $versionJson | grep -oP '"name":"(.*?)",' | grep -oP '[0-9\\.]+'`
 echo $current_version
+if [ "$build_version" != "$current_version" ]; then
+   #create new version
+   curl -X POST -u kjlubick:$API_KEY  -H "Content-Type: application/json" -d '{"name":"'"$build_version"'","desc":"For more release information, see https://github.com/kjlubick/fb-contrib-eclipse-quick-fixes/releases"}'  https://api.bintray.com/packages/kjlubick/fb-contrib-eclipse-quickfixes/fb-contrib-eclipse-quickfixes/versions
+
+fi	
