a6e5ba7c63a66035417ec651db265b941c9da076
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-28 18:35:05 -0500

    Improves deploy logging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 build_version=`cat version.txt`
-echo $build_version
+echo "build version = "$build_version
 versionJson=`curl -X GET -u kjlubick:$API_KEY https://api.bintray.com/packages/kjlubick/fb-contrib-eclipse-quickfixes/fb-contrib-eclipse-quickfixes/versions/_latest`
 #echo $versionJson
 current_version=`echo $versionJson | grep -oP '"name":"(.*?)",' | grep -oP '[0-9\\.]+'`
-echo $current_version
+echo "deployed version = "$current_version
 if [ "$build_version" != "$current_version" ]; then
    #create new version
    response=`curl -X POST -u kjlubick:$API_KEY  -H "Content-Type: application/json" -d '{"name":"'"$build_version"'","desc":"For more release information, see https://github.com/kjlubick/fb-contrib-eclipse-quick-fixes/releases"}'  https://api.bintray.com/packages/kjlubick/fb-contrib-eclipse-quickfixes/fb-contrib-eclipse-quickfixes/versions`
@@ -41,5 +41,6 @@ if [ "$build_version" != "$current_version" ]; then
    fi
    
    #curl -X POST -u kjlubick:$API_KEY https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/fb-contrib-eclipse-quickfixes/$current_version/publish
-
-fi	
+else
+   echo "Not pushing new version because we are on the same version as bintray"
+fi
