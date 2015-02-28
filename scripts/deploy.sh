b61d54dface0b7c488434a8389f96c96ca639ff4
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-28 11:07:48 -0500

    Fetches current version from bintray

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,6 @@
-version=`cat version.txt`
-echo $version
+build_version=`cat version.txt`
+echo $build_version
+versionJson=`curl -X GET -u kjlubick:$API_KEY https://api.bintray.com/packages/kjlubick/fb-contrib-eclipse-quickfixes/fb-contrib-eclipse-quickfixes/versions/_latest`
+#echo $versionJson
+current_version=`echo $versionJson | grep -oP '"name":"(.*?)",' | grep -oP '[0-9\\.]+'`
+echo $current_version
