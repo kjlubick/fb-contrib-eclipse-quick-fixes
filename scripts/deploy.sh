8b140b894ce59dbdfef518fc025695a6ab631a78
Kevin Lubick <kjlubick@ncsu.edu>
2015-02-28 21:21:40 -0500

    Fixes deploy version
    Was using the old version instead of the new version.
    Also enables publishing after uploading.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,6 @@
 build_version=`cat version.txt`
 echo "build version = "$build_version
 versionJson=`curl -X GET -u kjlubick:$API_KEY https://api.bintray.com/packages/kjlubick/fb-contrib-eclipse-quickfixes/fb-contrib-eclipse-quickfixes/versions/_latest`
-#echo $versionJson
 current_version=`echo $versionJson | grep -oP '"name":"(.*?)",' | grep -oP '[0-9\\.]+'`
 echo "deployed version = "$current_version
 if [ "$build_version" != "$current_version" ]; then
@@ -14,8 +13,18 @@ if [ "$build_version" != "$current_version" ]; then
    fi
    
    #updates metadata files
-   curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/artifacts.jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/artifacts.jar
-   curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/content.jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/content.jar
+   response=`curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/artifacts.jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/artifacts.jar`
+   echo $response
+   if [[ $response != *"success"* ]]; then
+      echo "Failed to upload output-site/artifacts.jar"
+      exit 11
+   fi
+   response=`curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/content.jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/content.jar`
+   echo $response
+   if [[ $response != *"success"* ]]; then
+      echo "Failed to upload output-site/content.jar"
+      exit 12
+   fi
    
    findbugs_plugin_jar=`ls -1 output-site/features/edu.umd.cs.findbugs.plugin.eclipse* | grep -o edu.*`
    eclipse_plugin_jar=`ls -1 output-site/features/fb-contrib-eclipse-quick-fixes* | grep -o fb-contrib.*`
@@ -23,24 +32,29 @@ if [ "$build_version" != "$current_version" ]; then
    echo $eclipse_plugin_jar
    
    #pushes findbugs plugin - probably already exists, but that's okay
-   curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/features/$findbugs_plugin_jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/features/$findbugs_plugin_jar -H "X-Bintray-Package: fb-contrib-eclipse-quickfixes" -H "X-Bintray-Version: $current_version"
-   curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/plugins/$findbugs_plugin_jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/plugins/$findbugs_plugin_jar -H "X-Bintray-Package: fb-contrib-eclipse-quickfixes" -H "X-Bintray-Version: $current_version"
+   curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/features/$findbugs_plugin_jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/features/$findbugs_plugin_jar -H "X-Bintray-Package: fb-contrib-eclipse-quickfixes" -H "X-Bintray-Version: $build_version"
+   curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/plugins/$findbugs_plugin_jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/plugins/$findbugs_plugin_jar -H "X-Bintray-Package: fb-contrib-eclipse-quickfixes" -H "X-Bintray-Version: $build_version"
    
    #pushes fb-contrib-eclipse-quickfixes plugin - fail if it doesn't exist
-   response=`curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/features/$eclipse_plugin_jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/features/$eclipse_plugin_jar -H "X-Bintray-Package: fb-contrib-eclipse-quickfixes" -H "X-Bintray-Version: $current_version"`
+   response=`curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/features/$eclipse_plugin_jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/features/$eclipse_plugin_jar -H "X-Bintray-Package: fb-contrib-eclipse-quickfixes" -H "X-Bintray-Version: $build_version"`
    echo $response
    if [[ $response != *"success"* ]]; then
       echo "Failed to upload output-site/features/"$eclipse_plugin_jar
-      exit 11
+      exit 13
    fi
-   response=`curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/plugins/$eclipse_plugin_jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/plugins/$eclipse_plugin_jar -H "X-Bintray-Package: fb-contrib-eclipse-quickfixes" -H "X-Bintray-Version: $current_version"`
+   response=`curl -X PUT -u kjlubick:$API_KEY --data-binary @output-site/plugins/$eclipse_plugin_jar https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/plugins/$eclipse_plugin_jar -H "X-Bintray-Package: fb-contrib-eclipse-quickfixes" -H "X-Bintray-Version: $build_version"`
    echo $response
    if [[ $response != *"success"* ]]; then
       echo "Failed to upload output-site/plugins/"$eclipse_plugin_jar
-      exit 12
+      exit 14
    fi
    
-   #curl -X POST -u kjlubick:$API_KEY https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/fb-contrib-eclipse-quickfixes/$current_version/publish
+   response=`curl -X POST -u kjlubick:$API_KEY https://api.bintray.com/content/kjlubick/fb-contrib-eclipse-quickfixes/fb-contrib-eclipse-quickfixes/$build_version/publish`
+   echo $response
+   if [[ $response != *"files"* ]]; then
+      echo "Failed to publish"
+      exit 15
+   fi
 else
    echo "Not pushing new version because we are on the same version as bintray"
 fi
