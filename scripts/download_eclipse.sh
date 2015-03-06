c80e0a74510b10eb4323d85f41019b89a93091d8
Kevin Lubick <kjlubick@ncsu.edu>
2015-03-06 00:04:44 -0500

    Trying to explicitly install the build plugins
    I thought they had been bundled, but apparently something was missed.

--- scripts/download_eclipse.sh
+++ scripts/download_eclipse.sh
@@ -8,4 +8,9 @@ tar -C $HOME -zxf $HOME/eclipse-tar/eclipse.tar.gz
 ls -lh $HOME
 ls -lh $HOME/eclipse
 
+$HOME/eclipse/eclipse -nosplash -application org.eclipse.equinox.p2.director     -repository http://download.eclipse.org/eclipse/updates/4.4/ -installIU org.eclipse.osgi.compatibility.plugins.feature.feature.group
+
+$HOME/eclipse/eclipse -nosplash -application org.eclipse.equinox.p2.director  -repository http://download.eclipse.org/eclipse/updates/4.4/ -installIU org.eclipse.pde.junit.runtime
+
+
 mv ./scripts/travisci.properties ./local.properties
