2ebefe0ffe856a07d00a2e262ed62279507ebaf2
Kevin Lubick <kjlubick@ncsu.edu>
2015-03-05 23:41:30 -0500

    Fixes download script

--- scripts/download_eclipse.sh
+++ scripts/download_eclipse.sh
@@ -1,10 +1,9 @@
 
 if [ ! -f "$HOME/eclipse-tar/eclipse.tar.gz" ]; then
-  mkdir $HOME/eclipse-tar
   wget https://github.com/kjlubick/fb-contrib-eclipse-quick-fixes/releases/download/0.4.0/eclipse-luna-linux-with-build-plugins.tar.gz -O $HOME/eclipse-tar/eclipse.tar.gz
   ls -lh $HOME/eclipse-tar
 fi
 
-gunzip -c $HOME/eclipse-tar/eclipse.tar.gz > $HOME
+tar -c $HOME -zxf $HOME/eclipse-tar/eclipse.tar.gz
 ls -lh $HOME
 ls -lh $HOME/eclipse
