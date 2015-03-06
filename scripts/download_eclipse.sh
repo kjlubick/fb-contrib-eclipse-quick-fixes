987d2a3e7433a323262aa5ae279a8fd02c9c891c
Kevin Lubick <kjlubick@ncsu.edu>
2015-03-06 00:18:39 -0500

    Trying from a clean eclipse

--- scripts/download_eclipse.sh
+++ scripts/download_eclipse.sh
@@ -1,12 +1,13 @@
 
 if [ ! -f "$HOME/eclipse-tar/eclipse.tar.gz" ]; then
-  wget https://github.com/kjlubick/fb-contrib-eclipse-quick-fixes/releases/download/0.4.0/eclipse-luna-linux-with-build-plugins.tar.gz -O $HOME/eclipse-tar/eclipse.tar.gz
+  wget https://github.com/kjlubick/eclipse-wercker-box/releases/download/0.0.1/eclipse-luna.tar.gz -O $HOME/eclipse-tar/eclipse.tar.gz
   ls -lh $HOME/eclipse-tar
 fi
 
 tar -C $HOME -zxf $HOME/eclipse-tar/eclipse.tar.gz
 ls -lh $HOME
 ls -lh $HOME/eclipse
+ls -lh $HOME/eclipse/eclipse
 
 $HOME/eclipse/eclipse -nosplash -application org.eclipse.equinox.p2.director     -repository http://download.eclipse.org/eclipse/updates/4.4/ -installIU org.eclipse.osgi.compatibility.plugins.feature.feature.group
 
