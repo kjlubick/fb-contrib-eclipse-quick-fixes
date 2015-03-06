69d646b1ea30d0886a516e33def30da3d0e64d57
Kevin Lubick <kjlubick@ncsu.edu>
2015-03-06 00:38:09 -0500

    Attempting to copy rt.jar
    Needed for tests to compile.

--- scripts/download_eclipse.sh
+++ scripts/download_eclipse.sh
@@ -7,11 +7,14 @@ fi
 tar -C $HOME -zxf $HOME/eclipse-tar/eclipse.tar.gz
 ls -lh $HOME
 ls -lh $HOME/eclipse
-ls -lh $HOME/eclipse/eclipse
 
+echo "Installing org.eclipse.osgi.compatibility.plugins.feature.feature.group"
 $HOME/eclipse/eclipse -nosplash -application org.eclipse.equinox.p2.director     -repository http://download.eclipse.org/eclipse/updates/4.4/ -installIU org.eclipse.osgi.compatibility.plugins.feature.feature.group
 
+echo "Installing org.eclipse.pde.junit.runtime"
 $HOME/eclipse/eclipse -nosplash -application org.eclipse.equinox.p2.director  -repository http://download.eclipse.org/eclipse/updates/4.4/ -installIU org.eclipse.pde.junit.runtime
 
 
 mv ./scripts/travisci.properties ./local.properties
+ls -rlh $JAVA_HOME
+cp $JAVA_HOME/lib/rt.jar ./testresources/rt17.jar
